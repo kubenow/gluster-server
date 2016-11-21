628cc38ce5b9b0473c7b5281c01d779c311bd3e0
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-11-21 09:54:19 +0100

    peers spaced

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Connect to the other servers
-peer_list=$(echo $GLUSTER_PEERS | sed 's/,/ /g')
+peer_list=$(echo $GLUSTER_PEERS)
 for peer in $peer_list ; do
   gluster peer probe $peer
 done
