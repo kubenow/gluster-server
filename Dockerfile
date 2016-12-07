b97e0e4d8c3e55ba0c39ed95d414fba10d4a7e23
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-12-07 13:39:22 +0100

    fix peer probe

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ FROM ubuntu:xenial
 RUN apt-get update -y
 RUN apt-get install -y \
   glusterfs-server \
-  attr
+  attr \
+  daemon
 
 # Install entrypoint
 ADD bin/entrypoint.sh /entrypoint.sh
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,5 +6,13 @@ service glusterfs-server start
 # Connect to the other servers
 for peer in $GLUSTER_PEERS ; do
   echo "Attempting peer probe $peer"
-  gluster peer probe $peer
+  wait_time=0
+  retrials=${PROBE_RETRIALS:-35} # Try for 10 minutes by default
+  until gluster peer probe $peer || [$wait_time -eq $retrials]; do
+    sleep_time=$(( wait_time++ ))
+    echo "Try again in $sleep_time s"
+    sleep $sleep_time
+  done
 done
+
+tail -f /dev/null # Hang on :-)
