7882547378a93215f5dd4b59984a9b22665ce90b
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-12-07 13:53:26 +0100

    fix entrypoint condition

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,7 +8,7 @@ for peer in $GLUSTER_PEERS ; do
   echo "Attempting peer probe $peer"
   wait_time=0
   retrials=${PROBE_RETRIALS:-35} # Try for 10 minutes by default
-  until gluster peer probe $peer || [$wait_time -eq $retrials]; do
+  until gluster peer probe $peer || [ $wait_time -eq $retrials ]; do
     sleep_time=$(( wait_time++ ))
     echo "Try again in $sleep_time s"
     sleep $sleep_time
